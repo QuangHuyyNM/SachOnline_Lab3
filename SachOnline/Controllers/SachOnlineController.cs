f53a6eb044022315dfaa95aa5c072e1c0d96f76c
QuangHuyyNM <huynm2307@gmail.com>
2023-10-11 23:56:54 +0700

    first commit

---
+++ SachOnline/Controllers/SachOnlineController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SachOnline.Models;
+
+namespace SachOnline.Controllers
+{
+    public class SachOnlineController : Controller
+    {
+        // GET: SachOnline
+        private string connection;
+        private dbSachOnlineDataContext data;
+        public SachOnlineController()
+        {
+            // Khởi tạo chuỗi kết nối
+            connection = "Data Source=LAPTOP-T9N6TG0M\\SQLEXPRESS;Initial Catalog=SachOnline;Integrated Security=True";
+            data = new dbSachOnlineDataContext(connection);
+        }
+        private List<SACH> LaySachMoi(int count)
+        {
+            return data.SACHes.OrderByDescending(a => a.NgayCapNhat).Take(count).ToList();
+        }
+        public ActionResult Index()
+        {
+                //Lay 6 quyen sach moi
+                var listSachMoi = LaySachMoi(6);
+                return View(listSachMoi);
+        }
+        public ActionResult ChuDePartial()
+        {
+            var listChuDe = from cd in data.CHUDEs select cd; 
+            return PartialView(listChuDe);
+        }
+        public ActionResult NhaXuatBanPartial()
+        {
+            var listNhaXuatBan = from nxb in data.NHAXUATBANs select nxb;
+            return PartialView(listNhaXuatBan);
+        }
+        public ActionResult SachTheoChuDe(int id)
+        {
+            var sach = from s in data.SACHes where s.MaCD == id select s;
+            return View(sach);
+        }
+        public ActionResult SachTheoNhaXuatBan(int id)
+        {
+            var sach = from s in data.SACHes where s.MaNXB == id select s;
+            return View(sach);
+        }
+        public ActionResult ChiTietSach(int id)
+        {
+            var sach = from s in data.SACHes where s.MaSach == id select s;
+            return View(sach.Single());
+        }
+    }
+}
+
+
+
